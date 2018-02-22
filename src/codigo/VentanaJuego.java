6a23cf2f5e5c0565710fb18c0885fd9713d2db86
Alberto Goujon <>
2018-02-22 18:08:05 +0100

    El bucle del juego esta funcionando

--- src/codigo/VentanaJuego.java
+++ src/codigo/VentanaJuego.java
@@ -3,9 +3,16 @@
 
  Ejercicio para explicar los siguientes conceptos:
  - Hilos de ejecución
- - Array List
+ - ArrayList
  */
 package codigo;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import javax.swing.Timer;
 
 /**
  *
@@ -13,11 +20,52 @@ package codigo;
  */
 public class VentanaJuego extends javax.swing.JFrame {
 
+    static int ANCHOPANTALLA = 600;
+    static int ALTOPANTALLA = 450;
+    
+    BufferedImage buffer = null;
+    
+    //Bucle de animación del juego
+    //En este caso es un hilo de ejecucuión nuevo que se encarga
+    //de refrescar la pantalla
+    
+    Timer temporizador = new Timer(10, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //TODO: codigo de la animación
+            bucleDelJuego();
+        }
+    });
+    
+    
     /**
      * Creates new form VentanaJuego
      */
     public VentanaJuego() {
         initComponents();
+        setSize(ANCHOPANTALLA, ALTOPANTALLA);
+        buffer = (BufferedImage)jPanel1.createImage(ANCHOPANTALLA, ALTOPANTALLA);
+        buffer.createGraphics();
+        //Inicio el temporizador
+        temporizador.start();
+    }
+    
+    private void bucleDelJuego()
+    {
+        //el bucle de animación gobierna el redibujado de los objetos en el jPanel1
+        //Primero borro todo la que haye en el bufer 
+        Graphics2D g2 = (Graphics2D) buffer.getGraphics();
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, ANCHOPANTALLA, ALTOPANTALLA);
+        
+        ////////////////////////////////////////////////////////////////////////
+        //Redibujamos cada elemento en su nueva posición  en el bufer
+        
+        ////////////////////////////////////////////////////////////////////////
+        //Dibujo de golpe el bufer sobre el jPanel1
+        g2 = (Graphics2D) jPanel1.getGraphics();
+        g2.drawImage(buffer, 0, 0, null);
+        
     }
 
     /**
@@ -29,17 +77,30 @@ public class VentanaJuego extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 720, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 519, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 519, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -81,5 +142,6 @@ public class VentanaJuego extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
